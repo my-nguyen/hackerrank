4946a4bf84991c9ad23a1be7b8c11b7159101d29
My Nguyen <nguyen_my@yahoo.com>
2017-03-29 15:33:39 -0700

    Update GLMBalancedParentheses.java

--- CTCI/GLMBalancedParentheses.java
+++ CTCI/GLMBalancedParentheses.java
@@ -1,6 +1,7 @@
 class GLMBalancedParentheses {
     public static char[][] TOKENS = { {'{', '}'}, {'[', ']'}, {'(', ')'} };
 
+    // whether a character is an open term: "{", "[", or "("
     public static boolean isOpenTerm(char c) {
         for (char[] array : TOKENS) {
             if (array[0] == c)
@@ -9,6 +10,8 @@ class GLMBalancedParentheses {
         return false;
     }
 
+    // whether openTerm is an open term and if so, whether it matches
+    // with a corresponding close term
     public static boolean matches(char openTerm, char closeTerm) {
         for (char[] array : TOKENS) {
             if (array[0] == openTerm)
@@ -17,16 +20,26 @@ class GLMBalancedParentheses {
         return false;
     }
 
+    // whether a string expression has all its open terms match with
+    // their corresponding close terms
     public static boolean isBalanced(String expression) {
+        // storage of all open terms encountered and not popped so far
         Stack<Character> stack = new Stack<>();
         for (char c : expression.toCharArray()) {
+            // if character is an open term, push it into stack
             if (isOpenTerm(c)) {
                 stack.push(c);
+            // if character is a close term, see if it matches with
+            // the last-pushed open term
             } else {
                 if (stack.isEmpty() || !matches(stacks.pop(), c))
                     return false;
             }
         }
+
+        // if there remains some elements (open terms) in the stack
+        // at this point, then the expression is not balanced.
+        // otherwise the expression is balanced.
         return stack.isEmpty();
     }
 }
