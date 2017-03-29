3b523892399cecf486a9712caef9c96a4b9449ba
My Nguyen <nguyen_my@yahoo.com>
2017-03-29 12:04:45 -0700

    Update GLMAnagram.java

--- CTCI/GLMAnagram.java
+++ CTCI/GLMAnagram.java
@@ -1,6 +1,10 @@
+// Two strings are anagrams if they both contain the same letters with same count, but with different order
 class GLMAnagram {
     public static int NUMBER_LETTERS = 26;
 
+    // compare the two arrays int[26]; at each letter, count the difference
+    // the sum of all the differences is the result, which is the number of letters
+    // to remove from one string to make it an analgram of another string.
     public static int getDelta(int[] array1, int[] array2) {
         if (array1.length != array2.length)
             return -1;
@@ -12,7 +16,11 @@ class GLMAnagram {
         return delta;
     }
 
+    // count the number of occurrences of each letter in string
     public static int[] getCharCounts(String s) {
+        // use int[26] to represent the 26 letters in the alphabet
+        // initially all elements in the int[26] are initialized to 0
+        // then the for loop will increment the count for the corresponding letter
         int[] charCounts = new int[NUMBER_LETTERS];
         for (int i = 0; i < s.length; i++) {
             char c = s.charAt(i);
