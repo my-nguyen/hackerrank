1df20f0983b150e09a0a2c9290c8f55c94435ea1
My Nguyen <nguyen_my@yahoo.com>
2017-03-29 15:21:06 -0700

    Update GLMLinkedListCycle.java

--- CTCI/GLMLinkedListCycle.java
+++ CTCI/GLMLinkedListCycle.java
@@ -5,11 +5,16 @@ class GLMLinkedListCycle {
         Node fast = head.next;
         Node slow = head;
         while (fast != null && fast.next != null && slow != null) {
+            // if fast pointer catches up with slow pointer, there's a loop
             if (fast == slow)
                 return true;
+
+            // fast pointer advances 2 nodes at a time
             fast = fast.next.next;
+            // slow pointer advances 1 node at a time
             slow = slow.next;
         }
+        // if fast pointer reaches null, then there's no loop
         return false;
     }
 }
