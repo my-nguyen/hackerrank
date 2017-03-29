20ba05257edde594fc67a614fff6cf612335063f
My Nguyen <nguyen_my@yahoo.com>
2017-03-29 13:40:01 -0700

    Update GLMRunningMedian.java

--- CTCI/GLMRunningMedian.java
+++ CTCI/GLMRunningMedian.java
@@ -1,5 +1,6 @@
 class GLMRunningMedian {
     public static void addNumber(int number, PriorityQueue<Integer> lowers, PriorityQueue<Integer> highers) {
+        // if lower half is empty, or the number is less than the max value of the lower heap
         if (lowers.size() == 0 || number < lowers.peek()) {
             lowers.add(number);
         } else {
@@ -11,7 +12,10 @@ class GLMRunningMedian {
         PriorityQueue<Integer> biggerHeap = lowers.size() > highers.size() ? lowers : highers;
         PriorityQueue<Integer> smallerHeap = lowers.size() > highers.size() ? highers : lowers;
 
+        // if the size of the 2 heaps differ by 2 (but shouldn't be more than 2)
         if (biggerHeap.size() - smallerHeap.size() >= 2) {
+            // remove the top element from the bigger-size heap
+            // and add that element to the smaller-size heap
             smallerHeap.add(biggerHeap.poll());
         }
     }
@@ -20,8 +24,10 @@ class GLMRunningMedian {
         PriorityQueue<Integer> biggerHeap = lowers.size() > highers.size() ? lowers : highers;
         PriorityQueue<Integer> smallerHeap = lowers.size() > highers.size() ? highers : lowers;
 
+        // if the size of the 2 heaps are the same, take the average of the 2 top elements
         if (biggerHeap.size() == smallerHeap.size()) {
             return ((double)biggerHeap.peek() + smallerHeap.peek()) / 2;
+        // else if the size of the heaps differ by 1, just take the top element of the bigger-size heap
         } else {
             return biggerHeap.peek();
         }
@@ -36,11 +42,17 @@ class GLMRunningMedian {
         });
         // min heap on the upper half
         PriorityQueue<Integer> highers = new PriorityQueue<Integer>();
+        // the running medians
         double[] medians = new double[array.length];
+
         for (int i = 0; i < array.length; i++) {
+            // extract the array element
             int number = array[i];
+            // add the element into either the lower heap or the higher heap
             addNumber(number, lowers, highers);
+            // make sure the size of the 2 heaps differ by 0 or 1
             rebalance(lowers, highers);
+            // get the running median so far
             medians[i] = getMedian(lowers, highers);
         }
         return medians;
