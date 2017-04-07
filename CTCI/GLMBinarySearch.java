4170cb6b05464e0b38a434c7d0c3ed7316850384
My Nguyen <nguyen_my@yahoo.com>
2017-04-07 12:27:32 -0700

    Update GLMBinarySearch.java

--- CTCI/GLMBinarySearch.java
+++ CTCI/GLMBinarySearch.java
@@ -1,2 +1,30 @@
 class Solution {
+    public static boolean binarySearchRecursive(int[] array, int x, int left, int right) {
+        if (left > right)
+            return false;
+        
+        int mid = (left + right) / 2;
+        if (array[mid] == x) {
+            return true;
+        } else if (x < array[mid]) {
+            return binarySearchRecursive(array, x, left, mid - 1);
+        } else {
+            return binarySearchRecursive(array, x, mid + 1, right);
+        }
+    }
+
+    public static boolean binarySearchIterative(int[] array, int x) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] == x) {
+                return true;
+            } else if (x < array[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
 }
