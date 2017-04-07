55654238f6d6dd34ffbcbae2019a6b6e2e6a32cd
My Nguyen <nguyen_my@yahoo.com>
2017-04-06 21:56:37 -0700

    Update GLMLonelyInteger.java

--- CTCI/GLMLonelyInteger.java
+++ CTCI/GLMLonelyInteger.java
@@ -1,2 +1,9 @@
 public class Solution {
+    private static int lonelyInteger(int[] array) {
+        int result = 0;
+        for (int value : array) {
+            result ^= value;
+        }
+        return result;
+    }
 }
