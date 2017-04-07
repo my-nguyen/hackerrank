bf1faf2a40c6cdab9a94400d6e0aac9be7ccec8e
My Nguyen <nguyen_my@yahoo.com>
2017-04-07 12:59:42 -0700

    Update GLMQueueTwoStacks.java

--- CTCI/GLMQueueTwoStacks.java
+++ CTCI/GLMQueueTwoStacks.java
@@ -1,6 +1,8 @@
 class GLMQueueTwoStacks {
     public static class MyQueue<T> {
+        // for enqueue(), so newest item stays at top of stack
         private Stack<T> stackNewestOnTop = new Stack<>();
+        // for dequeue() and peek(), so oldest item stays at top of stack
         private Stack<T> stackOldestOnTop = new Stack<>();
 
         // push new value into stackNewestOnTop
